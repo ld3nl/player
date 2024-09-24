501909cb8ede4dd7c57cf191f50623ddc77d9b57
Denys Kravchenko <denys.montreal@gmail.com>
2024-09-23 18:28:25 -0700

    Refactor fetchPosts to retrieve all posts instead of 99 for improved performance

--- lib/server/fetchPosts.ts
+++ lib/server/fetchPosts.ts
@@ -56,7 +56,6 @@ export async function fetchPosts(): Promise<HomeProps> {
   const categoriesCount = await getCategoryCount(ROOT_CATEGORY_ID);
   const totalPosts = categoriesCount;
 
-  const ninetyNinePost = await getAllPostsFromServer(ROOT_CATEGORY_ID, 30, 0);
   // Calculate the number of API requests based on the total number of posts.
   const numberOfRequests = Math.ceil(totalPosts / 99);
 
@@ -81,7 +80,7 @@ export async function fetchPosts(): Promise<HomeProps> {
   });
 
   // Await all promises to resolve and flatten the result array.
-  const postsFromServer = await Promise.all(ninetyNinePost).then((results) =>
+  const postsFromServer = await Promise.all(promises).then((results) =>
     results
       .flat()
       .map(({ excerpt, title, date, id, categories, link, content }) =>
